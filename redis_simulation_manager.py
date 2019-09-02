7676211958371b36096d653b21e0861f1b602a44
mengyuan <myyan92@gmail.com>
2019-09-02 16:50:50 -0700

    tested redis_simulation_manager

--- redis_simulation_manager.py
+++ redis_simulation_manager.py
@@ -2,10 +2,9 @@ from redis_client import RedisQueue, RedisHash
 import json
 from physbam_python.rollout_physbam_3d import rollout_single as rollout_single_3d
 import numpy as np
-import signal
+import signal, sys
 from multiprocessing import Process, Value, cpu_count
 from ctypes import c_bool
-import pdb
 
 def process_func(task_queue, result_hash, terminate):
     try:
@@ -25,23 +24,11 @@ def process_func(task_queue, result_hash, terminate):
         terminate.value=True
 
 if __name__=="__main__":
-    num_workers = cpu_count() # - 4
+    redis_host = sys.argv[1]
+    num_workers = cpu_count() - 4
     workers = []
-    # TODO add lock in queue and hash?
-    task_queue = RedisQueue()
-    task_queue.clear()
-    state = np.zeros((64,3))
-    state[:,0]=np.linspace(0,1,64)
-    action = np.zeros((10,4))
-    action[:,1] = 0.02
-    physbam_args = ' -friction 0.1 -self_friction 0.1 -stiffen_linear 1.0 -stiffen_bending 1.0'
-    for i in range(4):
-        action[:,3]=i*0.1+0.1
-        job_id = 'JOB%d'%(i)
-        task_str = json.dumps({'state':state.tolist(),'action':action.tolist(),'physbam_args':physbam_args,'job_id':job_id})
-        task_queue.put(task_str)
-
-    result_hash = RedisHash()
+    task_queue = RedisQueue(host=redis_host)
+    result_hash = RedisHash(host=redis_host)
     result_hash.clear()
     terminate = Value(c_bool, False)
     for _ in range(num_workers):
