9f8205e88cc9cef158f93a4ae5b91b6fd92acbef
mengyuan <myyan92@gmail.com>
2019-09-30 13:52:34 -0700

    add backup code for using zmq

---
+++ test_zmq_rep.py
@@ -0,0 +1,18 @@
+import zmq
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    service = context.socket(zmq.REP)
+
+    service.connect("tcp://127.0.0.1:5560")
+    request = service.recv()
+    print(request)
+    service.send(b"start working 1!")
+    request = service.recv()
+    print(request)
+    service.send(b"start working 2!")
+    request = service.recv()
+    print(request)
+    service.send(b"start working 3!")
+
+    service.close()
