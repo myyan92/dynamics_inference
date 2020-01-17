9ac2e0d65528421e89e76458e550243eba857738
mengyuan <mengyuan@bohg-ws-3.stanford.edu>
2020-01-17 13:18:19 -0800

    new physbam3d interface using raw states, eval_rollout on new franka data.

--- dynamic_models.py
+++ dynamic_models.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os, shutil
 import gin
 from multiprocessing import Pool
 from physbam_python.rollout_physbam_2d import rollout_single as rollout_single_2d
@@ -33,7 +34,7 @@ class physbam_2d(object):
   def __init__(self, physbam_args=" -disable_collisions -stiffen_bending 100"):
     self.physbam_args = physbam_args
 
-  def execute(self, state, actions):
+  def execute(self, state, actions, **kwargs):
     """Execute action sequence and get end state.
     Args:
       state: (N,2) array representing the current state.
@@ -45,7 +46,7 @@ class physbam_2d(object):
                                physbam_args=self.physbam_args)
     return state
 
-  def execute_batch(self, state, actions):
+  def execute_batch(self, state, actions, **kwargs):
     """Execute in batch.
     Args:
       state: (N,2) array or a list of (N,2) array representing the current state.
@@ -67,21 +68,36 @@ class physbam_2d(object):
 class physbam_3d(object):
   def __init__(self, physbam_args=" -friction 0.176 -stiffen_linear 2.223 -stiffen_bending 0.218"):
     self.physbam_args = physbam_args
+    if os.path.isdir('./physbam_3d_springs_tmp'):
+        shutil.rmtree('./physbam_3d_springs_tmp')
+    os.mkdir('./physbam_3d_springs_tmp')
 
-  def execute(self, state, actions):
+  def execute(self, state, actions, return_traj=False, reset_spring=True, idx=0):
     """Execute action sequence and get end state.
     Args:
       current: (N,2) array representing the current state.
       actions: A list of (Int, array) tuple, where the int is
-          the action node, and the array is 2d vector of action.
+          the action node, and the array is 2d/3d vector of action.
     """
     # transform actions
-    actions = [[ac[1][0], ac[1][1], float(ac[0])/(state.shape[0]-1)] for ac in actions]
-    actions = np.array(actions)
-    state = rollout_single_3d(state, actions, physbam_args=' -dt 1e-3 ' + self.physbam_args)
+    moves = np.array([ac[1] for ac in actions])
+    nodes = np.array([[float(ac[0])/(state.shape[0]//2-1)] for ac in actions])
+    actions = np.concatenate([moves, nodes],axis=1)
+    assert(state.shape==(128,3)) # must be raw state
+    if not reset_spring:
+        if (not os.path.exists(os.path.join('./physbam_3d_springs_tmp', 'linear_%02d.txt'%(idx)))) or \
+           (not os.path.exists(os.path.join('./physbam_3d_springs_tmp', 'bending_%02d.txt'%(idx)))):
+            raise ValueError('no spring files saved')
+    state = rollout_single_3d(state, actions, physbam_args=' -dt 1e-3 ' + self.physbam_args,
+                              return_traj=return_traj, input_raw=True, return_raw=True,
+                              save_linear_spring=os.path.join('./physbam_3d_springs_tmp', 'linear_%02d.txt'%(idx)) if reset_spring else None,
+                              save_bending_spring=os.path.join('./physbam_3d_springs_tmp', 'bending_%02d.txt'%(idx)) if reset_spring else None,
+                              load_linear_spring=os.path.join('./physbam_3d_springs_tmp', 'linear_%02d.txt'%(idx)) if not reset_spring else None,
+                              load_bending_spring=os.path.join('./physbam_3d_springs_tmp', 'bending_%02d.txt'%(idx)) if not reset_spring else None
+                              )
     return state
 
-  def execute_batch(self, state, actions):
+  def execute_batch(self, state, actions, return_traj=False, reset_spring=True):
     """Execute in batch.
     Args:
       state: (N,2) array or a list of (N,2) array representing the current state.
@@ -92,11 +108,17 @@ class physbam_3d(object):
     else:
         assert(state.ndim==2)
         state = [state for a in actions]
+    return_traj = [return_traj for a in actions]
+    reset_spring = [reset_spring for a in actions]
+    idx = [i for i,a in enumerate(actions)]
     pool = Pool(8)
-    states = pool.starmap(self.execute, zip(state, actions))
+    states = pool.starmap(self.execute, zip(state, actions, return_traj, reset_spring, idx))
     pool.close()
     pool.join()
     return states
+
+  def close(self):
+    shutil.rmtree('./physbam_3d_springs_tmp')
 
 
 import tensorflow as tf
@@ -106,8 +128,8 @@ from neural_simulator.model_wrapper import Model
 @gin.configurable
 class neural_sim(object):
   def __init__(self, model_type, snapshot):
-    self.start = tf.placeholder(tf.float32, shape=[None, 64, 2])
-    self.action = tf.placeholder(tf.float32, shape=[None, 64, 2])
+    self.start = tf.placeholder(tf.float32, shape=[None, 64, 3])
+    self.action = tf.placeholder(tf.float32, shape=[None, 64, 5])
 
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth=True
@@ -119,21 +141,21 @@ class neural_sim(object):
     print("Warning: This model is hacked to work with state and action in robot coordinate.")
     print("It will convert input state and action to fit the distribution of training data")
 
-  def execute(self, state, actions):
-    state = state*np.array([-1.0, 1.0])+np.array([0.5, 0.0])
-    actions = [(a[0], np.array([-a[1][0], a[1][1]])) for a in actions]
+  def execute(self, state, actions, **kwargs):
+#    state = state*np.array([-1.0, 1.0])+np.array([0.5, 0.0])
+#    actions = [(a[0], np.array([-a[1][0], a[1][1]])) for a in actions]
 
     onehot_actions = []
     for ac in actions:
-      onehot_action = np.zeros_like(state)
+      onehot_action = np.zeros((state.shape[0], ac[1].shape[0]))
       onehot_action[ac[0],:] = ac[1]
       onehot_actions.append(onehot_action)
     for ac in onehot_actions:
       state = self.model.predict_single(self.sess, state, ac)
-    state[:,0]=0.5-state[:,0]
+ #   state[:,0]=0.5-state[:,0]
     return state
 
-  def execute_batch(self, state, actions):
+  def execute_batch(self, state, actions, **kwargs):
     if isinstance(state, list) or state.ndim==3:
         assert(len(state)==len(actions))
     else:
@@ -141,20 +163,40 @@ class neural_sim(object):
         state = [state for a in actions]
     state = np.array(state)
 
-    state = state*np.array([-1.0, 1.0])+np.array([0.5, 0.0])
-    actions = [[(a[0], np.array([-a[1][0], a[1][1]])) for a in action] for action in actions]
+#    state = state*np.array([-1.0, 1.0])+np.array([0.5, 0.0])
+#    actions = [[(a[0], np.array([-a[1][0], a[1][1]])) for a in action] for action in actions]
 
-    onehot_actions = []
-    num_steps = len(actions[0]) # assume it's the same for each
-    for t in range(num_steps):
-      onehot_ac = np.zeros_like(state)
+    num_steps = [len(action) for action in actions]
+    max_num_steps = np.amax(num_steps)
+
+    final_state = np.zeros_like(state)
+    for t in range(max_num_steps):
+      onehot_ac = np.zeros((state.shape[0], state.shape[1], actions[0][0][1].shape[0]+2))
       for i,action in enumerate(actions):
-        onehot_ac[i,action[t][0],:] = action[t][1]
-      onehot_actions.append(onehot_ac)
-    for ac in onehot_actions:
-      state = self.model.predict_batch(self.sess, state, ac)
-    state[:,:,0]=0.5-state[:,:,0]
-    return state
+        if t<num_steps[i]:
+          onehot_ac[i,action[t][0],:-2] = action[t][1]
+          # compute action angle feature.
+          start_ori = state[i, min(action[t][0]+1,state.shape[1]-1)] - state[i, max(action[t][0]-1,0)]
+          start_theta = np.arctan2(start_ori[1], start_ori[0])
+          if np.linalg.norm(action[t][1][:2]) > 3e-3:
+            target_theta = np.arctan2(action[t][1][1],action[t][1][0])
+            if action[t][0] <= 4:
+              target_theta += np.pi/2.0
+            elif action[t][0] > 59:
+              target_theta -= np.pi/2.0
+            elif np.dot(action[t][1][:2], start_ori[:2]) < 0.0:
+              target_theta += np.pi
+          else:
+            target_theta=start_theta
+
+          act_angle = np.array([np.sin(target_theta-start_theta), np.cos(target_theta-start_theta)])
+          onehot_ac[i,action[t][0],-2:] = act_angle
+      state = self.model.predict_batch(self.sess, state, onehot_ac)
+      for i,st in enumerate(state):
+        if t==num_steps[i]-1:
+          final_state[i] = st
+#    state[:,:,0]=0.5-state[:,:,0]
+    return final_state
 
 
 if __name__=='__main__':
--- eval_rollout.py
+++ eval_rollout.py
@@ -7,9 +7,7 @@ import argparse, gin
 from visual_inference.hybrid_inference import HybridInference
 from TF_cloth2d.models.model_VGG_STN_2 import Model_STNv2
 from dynamics_inference.dynamic_models import *
-
-
-inferencer = HybridInference()
+from physbam_python.state_to_mesh import state_to_mesh
 
 def load_sim_sequence(dir, use_vision=False):
     index = dir.split('/')[-1]
@@ -25,17 +23,17 @@ def load_sim_sequence(dir, use_vision=False):
         state[:,1]=-state[:,1] # for seq3d data
         states.append(state)
     if use_vision:
-        im = Image.open(os.path.join(dir, index+'_000.png'))
+        im = Image.open(os.path.join(dir, index+'_001.png'))
         image = np.array(im)[::-1,:,:] # for seq3d data
         inferencer.fitting.init_position=None # reset memory to be safe
         input_state = inferencer.inference(image)
         # convert frame
         # align to true state
     else:
-        input_state = states[0]
+        input_state = states[1]
     return input_state, states[1:], actions[1:]
 
-def load_real_sequence(dir):
+def load_real_sequence_old(dir):
     inferencer.fitting.init_position=None # reset memory
     actions = np.load(os.path.join(dir, 'actions.npy'))
     actions = actions[:,[12,13,16,17]]
@@ -56,6 +54,33 @@ def load_real_sequence(dir):
 
     return states[0], states, converted_actions
 
+def load_real_sequence_new(dir):
+    # for real_rope_with_occlusion-new
+    data = np.load(os.path.join(dir, 'history_perception.npz'))
+    states = data['perception']
+    actions = data['actions'] # array of objects
+
+    converted_actions = []
+    fully_observe = []
+    action_node = None
+    start_position = None
+    for act, state in zip(actions, states):
+        if len(act)==4:
+            dists = np.linalg.norm(state-act[:2], axis=-1)
+            action_node = np.argmin(dists)
+            if start_position is not None:
+                converted_actions.append((1,np.array([0.0,0.0]))) # mock release
+            start_position = state[action_node]
+            fully_observe.append(True)
+        else:
+            move = act[:2]-start_position
+            converted_actions.append((action_node, move))
+            start_position = act[:2]
+            fully_observe.append(False)
+    converted_actions.append((1,np.array([0.0,0.0])))
+    fully_observe.append(True)
+    return states[0], states, converted_actions, fully_observe
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', help='type of model to evaluate')
@@ -66,6 +91,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     gin.parse_config_files_and_bindings([args.gin_config], args.gin_bindings)
+
+    inferencer = HybridInference()
 
     if args.model == 'neural':
         simulator = neural_sim()
@@ -94,35 +121,57 @@ if __name__ == "__main__":
     if args.data == 'train':
         for i in train_index[:200]:
             input_state, states, actions = \
-                load_sim_sequence('/scr1/mengyuan/data/sim3d_sequence/%04d'%(i), use_vision=True)
+                load_sim_sequence('/scr1/mengyuan/data/sim3d_sequence/%04d'%(i), use_vision=False)
             batch_gt_states.append(states[:59])
             batch_actions.append(actions[:58])
             batch_input_states.append(input_state)
     elif args.data == 'test':
         for i in test_index[:200]:
             input_state, states, actions = \
-                load_sim_sequence('/scr1/mengyuan/data/sim3d_sequence/%04d'%(i), use_vision=True)
+                load_sim_sequence('/scr1/mengyuan/data/sim3d_sequence/%04d'%(i), use_vision=False)
             batch_gt_states.append(states[:59])
             batch_actions.append(actions[:58])
             batch_input_states.append(input_state)
     elif args.data == 'real':
         inferencer.memory = True
-        for i in [1,2,3,5,8,9]:
-            input_state, states, actions = \
-                load_real_sequence('/scr1/mengyuan/data/real_rope_ours_2/seq_m%d_2'%(i))
-            batch_gt_states.append(states[:59])
-            batch_actions.append(actions[:58])
-            batch_input_states.append(input_state)
+        for i in range(1,28):
+            if os.path.isdir('/scr1/mengyuan/data/real_rope_with_occlusion-new/run_%d'%(i)):
+                input_state, states, actions, fully_observe = \
+                    load_real_sequence_new('/scr1/mengyuan/data/real_rope_with_occlusion-new/run_%d'%(i))
+                batch_gt_states.append(states[:89])
+                batch_actions.append(actions[:88])
+                batch_input_states.append(input_state)
 
     print("load time: ", time.time()-start_time)
     start_time = time.time()
 
+    state = []
+    # to be tested for the new physbam_3d sim
+    if args.model=='physbam_3d':
+        idx = []
+        for i,bis in enumerate(batch_input_states):
+            try:
+                st = state_to_mesh(bis)
+                st = st.dot(np.array([[1,0,0],[0,0,1],[0,-1,0]]))
+                state.append(st)
+                idx.append(i)
+            except NotImplementedError:
+                print("sequence %d cannot construct mesh" %(i))
+        batch_gt_states = [batch_gt_states[i] for i in idx]
+        batch_actions = [batch_actions[i] for i in idx]
+        batch_input_states = [batch_input_states[i] for i in idx]
+
     batch_gen_states = [batch_input_states]
-    state = batch_input_states
-    for i in range(58):
+    for i in range(len(batch_actions[0])):
         action = [[ac[i]] for ac in batch_actions]
-        state = simulator.execute_batch(state, action)
-        batch_gen_states.append(state)
+        state = simulator.execute_batch(state, action, reset_spring=(i==0))
+        if args.model=='physbam_3d':
+            obs = [0.5*(st[:64]+st[64:]) for st in state]
+            if batch_input_states[0].shape[-1]==2:
+                obs = [ob[:,:2] for ob in obs]
+            batch_gen_states.append(obs)
+        else:
+            batch_gen_states.append(state)
     batch_gen_states = np.array(batch_gen_states).transpose((1,0,2,3))
     print("prediction time: ", time.time()-start_time)
     start_time = time.time()
@@ -140,16 +189,16 @@ if __name__ == "__main__":
 
     plt.figure() # without offseting the start dists.
     plt.plot(np.mean(avg_dists, axis=0), c='C0')
-    plt.fill_between(np.arange(59),
+    plt.fill_between(np.arange(batch_gen_states.shape[1]),
                      np.mean(avg_dists, axis=0)-np.std(avg_dists, axis=0),
                      np.mean(avg_dists, axis=0)+np.std(avg_dists, axis=0),
                      alpha = 0.3, facecolor='C0')
     plt.plot(np.mean(max_dists, axis=0), c='C1')
-    plt.fill_between(np.arange(59),
+    plt.fill_between(np.arange(batch_gen_states.shape[1]),
                      np.mean(max_dists, axis=0)-np.std(max_dists, axis=0),
                      np.mean(max_dists, axis=0)+np.std(max_dists, axis=0),
                      alpha = 0.3, facecolor='C1')
-    plt.axis([0,60,0,0.5])
+    plt.axis([0,batch_gen_states.shape[1],0,0.5])
     plt.savefig(args.output+'.png') #('eval_sim_trainset_withvision.png')
     np.savez(args.output+'.npz', #('eval_sim_trainset_withvision.npz',
              mean_avg=np.mean(avg_dists, axis=0),
