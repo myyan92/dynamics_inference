466b77491c5306ce75ead9ede6e69add401bc362
mengyuan <mengyuan@bohg-ws-3.stanford.edu>
2020-02-09 20:49:39 -0800

    support return_traj for all dynamic models

--- dynamic_models.py
+++ dynamic_models.py
@@ -34,19 +34,24 @@ class physbam_2d(object):
   def __init__(self, physbam_args=" -disable_collisions -stiffen_bending 100"):
     self.physbam_args = physbam_args
 
-  def execute(self, state, actions, **kwargs):
+  def execute(self, state, actions,  return_traj=False, **kwargs):
     """Execute action sequence and get end state.
     Args:
       state: (N,2) array representing the current state.
       actions: A list of (Int, array) tuple, where the int is
           the action node, and the array is 2d vector of action.
     """
+    states = []
     for ac in actions:
       state = rollout_single_2d(state, ac[0]+1, ac[1], frames=1,
                                physbam_args=self.physbam_args)
-    return state
+      states.append(state)
+    if return_traj:
+      return states
+    else:
+      return state
 
-  def execute_batch(self, state, actions, **kwargs):
+  def execute_batch(self, state, actions, return_traj=False, **kwargs):
     """Execute in batch.
     Args:
       state: (N,2) array or a list of (N,2) array representing the current state.
@@ -57,8 +62,9 @@ class physbam_2d(object):
     else:
         assert(state.ndim==2)
         state = [state for a in actions]
-    pool = Pool(8)
-    states = pool.starmap(self.execute, zip(state, actions))
+    return_trajs = [return_traj for a in actions]
+    pool = Pool()
+    states = pool.starmap(self.execute, zip(state, actions, return_trajs))
     pool.close()
     pool.join()
     return states
@@ -111,7 +117,7 @@ class physbam_3d(object):
     return_traj = [return_traj for a in actions]
     reset_spring = [reset_spring for a in actions]
     idx = [i for i,a in enumerate(actions)]
-    pool = Pool(8)
+    pool = Pool()
     states = pool.starmap(self.execute, zip(state, actions, return_traj, reset_spring, idx))
     pool.close()
     pool.join()
@@ -129,7 +135,7 @@ from neural_simulator.model_wrapper import Model
 class neural_sim(object):
   def __init__(self, model_type, snapshot):
     self.start = tf.placeholder(tf.float32, shape=[None, 64, 3])
-    self.action = tf.placeholder(tf.float32, shape=[None, 64, 5])
+    self.action = tf.placeholder(tf.float32, shape=[None, 64, 3])
 
     tf_config = tf.ConfigProto()
     tf_config.gpu_options.allow_growth=True
@@ -141,7 +147,7 @@ class neural_sim(object):
     print("Warning: This model is hacked to work with state and action in robot coordinate.")
     print("It will convert input state and action to fit the distribution of training data")
 
-  def execute(self, state, actions, **kwargs):
+  def execute(self, state, actions, return_traj=False, **kwargs):
 #    state = state*np.array([-1.0, 1.0])+np.array([0.5, 0.0])
 #    actions = [(a[0], np.array([-a[1][0], a[1][1]])) for a in actions]
 
@@ -150,12 +156,17 @@ class neural_sim(object):
       onehot_action = np.zeros((state.shape[0], ac[1].shape[0]))
       onehot_action[ac[0],:] = ac[1]
       onehot_actions.append(onehot_action)
+    states = []
     for ac in onehot_actions:
       state = self.model.predict_single(self.sess, state, ac)
+      states.append(state)
  #   state[:,0]=0.5-state[:,0]
-    return state
+    if return_traj:
+      return states
+    else:
+      return state
 
-  def execute_batch(self, state, actions, **kwargs):
+  def execute_batch(self, state, actions, return_traj=False, **kwargs):
     if isinstance(state, list) or state.ndim==3:
         assert(len(state)==len(actions))
     else:
@@ -168,35 +179,42 @@ class neural_sim(object):
 
     num_steps = [len(action) for action in actions]
     max_num_steps = np.amax(num_steps)
-
+    state_trajs = [[] for a in actions]
     final_state = np.zeros_like(state)
     for t in range(max_num_steps):
-      onehot_ac = np.zeros((state.shape[0], state.shape[1], actions[0][0][1].shape[0]+2))
+#      onehot_ac = np.zeros((state.shape[0], state.shape[1], actions[0][0][1].shape[0]+2))
+      onehot_ac = np.zeros((state.shape[0], state.shape[1], actions[0][0][1].shape[0]))
       for i,action in enumerate(actions):
         if t<num_steps[i]:
-          onehot_ac[i,action[t][0],:-2] = action[t][1]
+#          onehot_ac[i,action[t][0],:-2] = action[t][1]
+          onehot_ac[i,action[t][0],:] = action[t][1]
           # compute action angle feature.
-          start_ori = state[i, min(action[t][0]+1,state.shape[1]-1)] - state[i, max(action[t][0]-1,0)]
-          start_theta = np.arctan2(start_ori[1], start_ori[0])
-          if np.linalg.norm(action[t][1][:2]) > 3e-3:
-            target_theta = np.arctan2(action[t][1][1],action[t][1][0])
-            if action[t][0] <= 4:
-              target_theta += np.pi/2.0
-            elif action[t][0] > 59:
-              target_theta -= np.pi/2.0
-            elif np.dot(action[t][1][:2], start_ori[:2]) < 0.0:
-              target_theta += np.pi
-          else:
-            target_theta=start_theta
+#          start_ori = state[i, min(action[t][0]+1,state.shape[1]-1)] - state[i, max(action[t][0]-1,0)]
+#          start_theta = np.arctan2(start_ori[1], start_ori[0])
+#          if np.linalg.norm(action[t][1][:2]) > 3e-3:
+#            target_theta = np.arctan2(action[t][1][1],action[t][1][0])
+#            if action[t][0] <= 4:
+#              target_theta += np.pi/2.0
+#            elif action[t][0] > 59:
+#              target_theta -= np.pi/2.0
+#            elif np.dot(action[t][1][:2], start_ori[:2]) < 0.0:
+#              target_theta += np.pi
+#          else:
+#            target_theta=start_theta
 
-          act_angle = np.array([np.sin(target_theta-start_theta), np.cos(target_theta-start_theta)])
-          onehot_ac[i,action[t][0],-2:] = act_angle
+#          act_angle = np.array([np.sin(target_theta-start_theta), np.cos(target_theta-start_theta)])
+#          onehot_ac[i,action[t][0],-2:] = act_angle
       state = self.model.predict_batch(self.sess, state, onehot_ac)
       for i,st in enumerate(state):
         if t==num_steps[i]-1:
           final_state[i] = st
+        if t<=num_steps[i]-1:
+          state_trajs[i].append(st)
 #    state[:,:,0]=0.5-state[:,:,0]
-    return final_state
+    if return_traj:
+      return state_trajs
+    else:
+      return final_state
 
 
 if __name__=='__main__':
